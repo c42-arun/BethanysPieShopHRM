8f6135e4008e263e813925b5e24152c1162ed941
Arun Ramachandriya <Arun.Ramachandriya@investec.co.uk>
2021-07-15 10:38:45 +0100

    Call JS component from Blazor

--- BethanysPieShopHRM.App/Pages/EmployeeDetail.cs
+++ BethanysPieShopHRM.App/Pages/EmployeeDetail.cs
@@ -1,4 +1,5 @@
 ﻿using BethanysPieShopHRM.App.Services;
+using BethanysPieShopHRM.ComponentsLibrary.Map;
 using BethanysPieShopHRM.Shared;
 using Microsoft.AspNetCore.Components;
 using System;
@@ -15,9 +16,17 @@ namespace BethanysPieShopHRM.App.Pages
         public Employee Employee { get; set; } = new Employee();
         [Inject]
         public IEmployeeDataService EmployeeDataService { get; set; }
+
+        public List<Marker> MapMarkers { get; set; } = new List<Marker>();
+
         protected override async Task OnInitializedAsync()
         {
             Employee = await EmployeeDataService.GetEmployeeDetails(int.Parse(EmployeeId));
+
+            MapMarkers = new List<Marker>
+            {
+                new Marker{Description = $"{Employee.FirstName} {Employee.LastName} {Employee.Longitude}-{Employee.Latitude}", ShowPopup = false, X = Employee.Longitude, Y = Employee.Latitude}
+            };
         }
     }
 }
